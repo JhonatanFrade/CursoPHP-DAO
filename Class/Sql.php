917ca83607a0ea5e58554ad0f90f4a7b18db85a2
Jhonatan Frade <jhone.rondonia@gmail.com>
2017-12-30 20:00:07 -0200

    inicio do projeto

---
+++ Class/Sql.php
@@ -0,0 +1,36 @@
+<?php
+
+class sql extends PDO {
+
+    private $conn;
+
+    public function __construct(){
+        $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
+    }
+
+
+    private function setParams($statment, $parameters = array()){
+        foreach ($parameters as $key => $value) {     
+            $this->setParam($key, $value);
+        }
+    }
+
+    private function setParam($statment, $key, $value){
+        $statment->bidParam($key, $value);
+    }
+
+    public function query($RawQuery, $params = array()){
+        $stmt = $this->conn->prepare($RawQuery);
+        $this->setParams($stmt, $params);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    // AGORA UM METODO PRO SELECT
+    public function select($RawQuery, $params = array()):array{
+        $stmt = $this->query($RawQuery, $params);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+?>
