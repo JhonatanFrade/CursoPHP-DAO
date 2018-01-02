b29c71fa6ac15fb0b6c56d3e7fb060b0332684bb
Jhonatan Frade <jhone.rondonia@gmail.com>
2018-01-02 00:25:58 -0200

    Atualizando o codigo

--- index.php
+++ index.php
@@ -4,17 +4,28 @@ require_once("config.php");
 
 /*
 $sql = new Sql();
-
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 echo json_encode($usuarios);
 */
 
-// SO USAR CLASS USUARIO
-
+// carrega um usuário
+/*
 $root = new Usuario();
-
 $root->loadById(4);
-
 echo $root;
+*/
+
+//carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root","!@#$");
+echo $usuario;
+
 ?>
